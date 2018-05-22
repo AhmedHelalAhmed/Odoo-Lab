09b184cfcf9f0035675923928812a2ca4b8d71b1
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-05-23 01:20:59 +0200

    add relations and approved by

--- custom/mylabp/models/department.py
+++ custom/mylabp/models/department.py
@@ -5,3 +5,4 @@ class Department(models.Model):
     _name = "mylab.department"
     name = fields.Char()
     num_of_machines = fields.Integer()
+    machine_ids = fields.One2many('mylab.machine','department_id' , string="Machines")
--- custom/mylabp/models/machine.py
+++ custom/mylabp/models/machine.py
@@ -1,10 +1,14 @@
 from odoo import models, fields
 
+
 class Machine(models.Model):
-    _name="mylab.machine"
+    _name = "mylab.machine"
     name = fields.Char()
     description = fields.Text()
     price = fields.Float()
     image = fields.Binary()
+    is_accepted = fields.Boolean(string="Is Accepted")
+    department_id = fields.Many2one('mylab.department', string="Department")
+    user_id = fields.Many2one('res.users', string='Accepted By',readonly=True,  default=lambda self: self.env.uid)
 
- 
+
