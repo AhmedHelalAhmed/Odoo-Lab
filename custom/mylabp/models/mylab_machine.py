341e5f694e4a8ffa75a869df7198f6218afd4d07
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-05-26 13:16:58 +0200

    add state with all functionality for buttons

--- custom/mylabp/models/mylab_machine.py
+++ custom/mylabp/models/mylab_machine.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class MyLabMachine(models.Model):
@@ -9,7 +9,26 @@ class MyLabMachine(models.Model):
     image = fields.Binary()
     is_accepted = fields.Boolean(string="Is Accepted")
     department_id = fields.Many2one('mylab.department', string="Department")
-    user_id = fields.Many2one('res.users', string='Approved By',default=lambda self: self.env.uid)
-    histories_ids = fields.One2many('mylab.history','machine_id', string='History')
-    tags_ids = fields.Many2many('mylab.tag',string="tags")
+    user_id = fields.Many2one('res.users', string='Approved By', default=lambda self: self.env.uid)
+    histories_ids = fields.One2many('mylab.history', 'machine_id', string='History')
+    tags_ids = fields.Many2many('mylab.tag', string="tags")
+    state = fields.Selection([
+        ('draft', "Draft"),
+        ('approve', "Approved"),
+        ('refuse', "Refused"),
+    ])
 
+    @api.multi
+    def draft(self):
+        for record in self:
+            record.state = "draft"
+
+    @api.multi
+    def approve(self):
+        for record in self:
+            record.state = "approve"
+
+    @api.multi
+    def refuse(self):
+        for record in self:
+            record.state = "refuse"
