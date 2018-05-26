6648c4425beabb1ca31057cb0d570c8f35489ea4
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-05-26 17:10:26 +0200

    add an other way to compute number of machines in run time

--- custom/mylabp/models/mylab_department.py
+++ custom/mylabp/models/mylab_department.py
@@ -1,8 +1,15 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class MyLabDepartment(models.Model):
     _name = "mylab.department"
     name = fields.Char()
-    num_of_machines = fields.Integer()
-    machines_ids = fields.One2many('mylab.machine','department_id' , string="Machines")
+    machines_ids = fields.One2many('mylab.machine', 'department_id', string="Machines")
+
+    # This is an other way and better than the handle in machine models
+    @api.multi
+    def _calc_machines(self):
+        for record in self:
+            record.num_of_machines = len(record.machines_ids)
+
+    num_of_machines = fields.Integer(compute=_calc_machines)
--- custom/mylabp/models/mylab_machine.py
+++ custom/mylabp/models/mylab_machine.py
@@ -68,4 +68,3 @@ class MyLabMachine(models.Model):
 
 
 
-
